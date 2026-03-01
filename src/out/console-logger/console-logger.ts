b8ddb78d233ee3de5116d1e3f37a02a6d7606e54
Fenix <fenixxx1990@yandex.ru>
2026-03-01 14:33:08 +0400

    command executer

--- src/out/console-logger/console-logger.ts
+++ src/out/console-logger/console-logger.ts
@@ -2,6 +2,7 @@ import { IStreamLogger } from "../../core/handlers/stream-logger.interface";
 
 export class ConsoleLogger implements IStreamLogger {
   private static logger: ConsoleLogger;
+
   public static getInstance() {
     if (!ConsoleLogger.logger) {
       ConsoleLogger.logger = new ConsoleLogger();
